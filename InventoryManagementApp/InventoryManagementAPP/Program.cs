f1e7357829fc5b7b2a9a77930c5e0ff7f013db13
Ivor Ćelić <ivorcelic@gmail.com>
2024-02-28 12:48:11 +0100

    Added final project and implemented documentation for it

---
+++ InventoryManagementApp/InventoryManagementAPP/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Needed for documentation
+builder.Services.AddSwaggerGen(sgo =>
+{
+    var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Inventory Management API",
+        Version = "v1",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Email = "ivorcelic@gmail.com",
+            Name = "Ivor Ćelić"
+        },
+        Description = "This is documentation for Inventory Management API",
+        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+        {
+            Name = "Education licence"
+        }
+
+    };
+    sgo.SwaggerDoc("v1", o);
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    sgo.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
