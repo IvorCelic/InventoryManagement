4afa962373545397493efd467ecc3f9e77e37e32
Ivor Ćelić <ivorcelic@gmail.com>
2024-03-22 13:41:03 +0100

    Fetching transactions working

--- Frontend/src/pages/transactions/Transactions.jsx
+++ Frontend/src/pages/transactions/Transactions.jsx
@@ -1,17 +1,61 @@
+import { useEffect, useState } from "react";
 import { Container, Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import TransactionService from "../../services/TransactionService";
+import moment from "moment";
 
 export default function Transactions() {
-    return(
+    const [transactions, setTransactions] = useState();
+    let navigate = useNavigate();
+
+    async function fetchTransactions() {
+        await TransactionService.get()
+            .then((res) => {
+                setTransactions(res.data);
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    }
+
+    useEffect(() => {
+        fetchTransactions();
+    }, []);
+
+    function formatDate(transactionDate) {
+        let mdp = moment.utc(transactionDate);
+        if (mdp.hour() == 0 && mdp.minutes() == 0) {
+            return mdp.format("DD. MM. YYYY.");
+        }
+        return mdp.format("DD. MM. YYYY. HH:mm");
+    }
+
+    return (
         <Container>
-            <Table>
+            <Table striped bordered hover responsive>
                 <thead>
-                    <th>Test</th>
+                    <tr>
+                        <th>Transaction date</th>
+                        <th>Status</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>test</td>
-                        <td>test</td>
-                    </tr>
+                    {transactions &&
+                        transactions.map((inventoryTransaction, index) => (
+                            <tr key={index}>
+                                <td>
+                                    {inventoryTransaction.transactionDate ==
+                                    null
+                                        ? 'Not defined'
+                                        : formatDate(
+                                              inventoryTransaction.transactionDate
+                                          )}
+                                </td>
+                                <td>
+                                    {inventoryTransaction.transactionStatusName}
+                                </td>
+                            </tr>
+                        ))}
                 </tbody>
             </Table>
         </Container>
