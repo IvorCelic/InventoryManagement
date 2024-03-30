1ed5e09591870871bf4a7b86c3d0ed16a34a60af
Ivor Ćelić <ivorcelic@gmail.com>
2024-03-30 20:46:16 +0100

    Ternary operators for rendering conditions

--- Frontend/src/components/Transactions/TransactionClosed.jsx
+++ Frontend/src/components/Transactions/TransactionClosed.jsx
@@ -6,6 +6,7 @@ function TransactionClosed({
     handleTabChange,
     associatedWarehouses,
     products,
+    warehouses,
     productsOnWarehouse,
     isUnitary,
 }) {
@@ -17,95 +18,108 @@ function TransactionClosed({
     };
 
     return (
-        <Col lg={8} md={12} sm={12} className="mt-5 transactionEditContainer">
-            <Row className="horizontal-tabs-container">
-                <Nav className="horizontal-tabs d-none d-md-flex">
-                    <Nav.Item>
-                        <Nav.Link
-                            eventKey="all"
-                            active={activeTab === "all"}
-                            onClick={() => handleWarehouseSelect("all", "All warehouses")}
-                        >
-                            All
-                        </Nav.Link>
-                    </Nav.Item>
-                    {associatedWarehouses &&
-                        associatedWarehouses.map((warehouse) => (
-                            <Nav.Item key={warehouse.id}>
+        <>
+            {associatedWarehouses && associatedWarehouses.length > 0 ? (
+                <Col lg={8} md={12} sm={12} className="mt-5 transactionEditContainer">
+                    <Row className="horizontal-tabs-container">
+                        <Nav className="horizontal-tabs d-none d-md-flex">
+                            <Nav.Item>
                                 <Nav.Link
-                                    eventKey={warehouse.id}
-                                    active={activeTab === warehouse.id}
-                                    onClick={() =>
-                                        handleWarehouseSelect(warehouse.id, warehouse.warehouseName)
-                                    }
+                                    eventKey="all"
+                                    active={activeTab === "all"}
+                                    onClick={() => handleWarehouseSelect("all", "All warehouses")}
                                 >
-                                    {warehouse.warehouseName}
+                                    All
                                 </Nav.Link>
                             </Nav.Item>
-                        ))}
-                </Nav>
-                <Nav className="horizontal-tabs d-md-none">
-                    <Nav.Item>
-                        <NavDropdown title={selectedWarehouseName} id="nav-dropdown">
-                            <NavDropdown.Item
-                                eventKey="all"
-                                active={activeTab === "all"}
-                                onClick={() => handleWarehouseSelect("all", "All warehouses")}
-                            >
-                                All warehouses
-                            </NavDropdown.Item>
                             {associatedWarehouses &&
                                 associatedWarehouses.map((warehouse) => (
+                                    <Nav.Item key={warehouse.id}>
+                                        <Nav.Link
+                                            eventKey={warehouse.id}
+                                            active={activeTab === warehouse.id}
+                                            onClick={() =>
+                                                handleWarehouseSelect(
+                                                    warehouse.id,
+                                                    warehouse.warehouseName
+                                                )
+                                            }
+                                        >
+                                            {warehouse.warehouseName}
+                                        </Nav.Link>
+                                    </Nav.Item>
+                                ))}
+                        </Nav>
+                        <Nav className="horizontal-tabs d-md-none">
+                            <Nav.Item>
+                                <NavDropdown title={selectedWarehouseName} id="nav-dropdown">
                                     <NavDropdown.Item
-                                        key={warehouse.id}
-                                        eventKey={warehouse.id}
-                                        active={activeTab === warehouse.id}
+                                        eventKey="all"
+                                        active={activeTab === "all"}
                                         onClick={() =>
-                                            handleWarehouseSelect(
-                                                warehouse.id,
-                                                warehouse.warehouseName
-                                            )
+                                            handleWarehouseSelect("all", "All warehouses")
                                         }
                                     >
-                                        {warehouse.warehouseName}
+                                        All warehouses
                                     </NavDropdown.Item>
-                                ))}
-                        </NavDropdown>
-                    </Nav.Item>
-                </Nav>
-            </Row>
-            <Row className="mt-3">
-                <Col>
-                    <h5 className="mt-3">Products</h5>
-                    <Table striped bordered hover size="sm" className="table-sm">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Is Unitary</th>
-                                <th>Quantity</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {activeTab === "all"
-                                ? products.map((product, index) => (
-                                      <tr key={index}>
-                                          <td>{product.productName}</td>
-                                          <td>{isUnitary(product)}</td>
-                                          <td>Quantity</td>
-                                      </tr>
-                                  ))
-                                : productsOnWarehouse.map((product, index) => (
-                                      <tr key={index}>
-                                          <td>{product.productName}</td>
-                                          <td>{isUnitary(product)}</td>
-                                          <td>Quantity</td>
-                                      </tr>
-                                  ))}
-                        </tbody>
-                    </Table>
+                                    {associatedWarehouses &&
+                                        associatedWarehouses.map((warehouse) => (
+                                            <NavDropdown.Item
+                                                key={warehouse.id}
+                                                eventKey={warehouse.id}
+                                                active={activeTab === warehouse.id}
+                                                onClick={() =>
+                                                    handleWarehouseSelect(
+                                                        warehouse.id,
+                                                        warehouse.warehouseName
+                                                    )
+                                                }
+                                            >
+                                                {warehouse.warehouseName}
+                                            </NavDropdown.Item>
+                                        ))}
+                                </NavDropdown>
+                            </Nav.Item>
+                        </Nav>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col>
+                            <h5 className="mt-3">Products</h5>
+                            <Table striped bordered hover size="sm" className="table-sm">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Is Unitary</th>
+                                        <th>Quantity</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {activeTab === "all"
+                                        ? products.map((product, index) => (
+                                              <tr key={index}>
+                                                  <td>{product.productName}</td>
+                                                  <td>{isUnitary(product)}</td>
+                                                  <td>Quantity</td>
+                                              </tr>
+                                          ))
+                                        : productsOnWarehouse.map((product, index) => (
+                                              <tr key={index}>
+                                                  <td>{product.productName}</td>
+                                                  <td>{isUnitary(product)}</td>
+                                                  <td>Quantity</td>
+                                              </tr>
+                                          ))}
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
                 </Col>
-            </Row>
-        </Col>
+            ) : (
+                <Col lg={8} md={12} sm={12} className="mt-5 transactionEditContainer">
+                    <p>nothing</p>
+                </Col>
+            )}
+        </>
     );
 }
 
