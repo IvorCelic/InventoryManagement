454d80df34199cfca892acd87e9a2cd1ea1cd926
Ivor Ćelić <ivorcelic@gmail.com>
2024-03-30 01:58:17 +0100

    Making mobile-friendly editing transactions

--- Frontend/src/components/Transactions/TransactionClosed.jsx
+++ Frontend/src/components/Transactions/TransactionClosed.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Col, Nav, Row, Table } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Nav, NavDropdown, Row, Table } from "react-bootstrap";
 
 function TransactionClosed({
     activeTab,
@@ -9,15 +9,22 @@ function TransactionClosed({
     productsOnWarehouse,
     isUnitary,
 }) {
+    const [selectedWarehouseName, setSelectedWarehouseName] = useState("Select warehouse");
+
+    const handleWarehouseSelect = (warehouseId, warehouseName) => {
+        setSelectedWarehouseName(warehouseName);
+        handleTabChange(warehouseId);
+    };
+
     return (
         <Col lg={8} md={12} sm={12} className="mt-5 transactionEditContainer">
             <Row className="horizontal-tabs-container">
-                <Nav className="horizontal-tabs">
+                <Nav className="horizontal-tabs d-none d-md-flex">
                     <Nav.Item>
                         <Nav.Link
                             eventKey="all"
                             active={activeTab === "all"}
-                            onClick={() => handleTabChange("all")}
+                            onClick={() => handleWarehouseSelect("all", "All warehouses")}
                         >
                             All
                         </Nav.Link>
@@ -28,12 +35,43 @@ function TransactionClosed({
                                 <Nav.Link
                                     eventKey={warehouse.id}
                                     active={activeTab === warehouse.id}
-                                    onClick={() => handleTabChange(warehouse.id)}
+                                    onClick={() =>
+                                        handleWarehouseSelect(warehouse.id, warehouse.warehouseName)
+                                    }
                                 >
                                     {warehouse.warehouseName}
                                 </Nav.Link>
                             </Nav.Item>
                         ))}
+                </Nav>
+                <Nav className="horizontal-tabs d-md-none">
+                    <Nav.Item>
+                        <NavDropdown title={selectedWarehouseName} id="nav-dropdown">
+                            <NavDropdown.Item
+                                eventKey="all"
+                                active={activeTab === "all"}
+                                onClick={() => handleWarehouseSelect("all", "All warehouses")}
+                            >
+                                All warehouses
+                            </NavDropdown.Item>
+                            {associatedWarehouses &&
+                                associatedWarehouses.map((warehouse) => (
+                                    <NavDropdown.Item
+                                        key={warehouse.id}
+                                        eventKey={warehouse.id}
+                                        active={activeTab === warehouse.id}
+                                        onClick={() =>
+                                            handleWarehouseSelect(
+                                                warehouse.id,
+                                                warehouse.warehouseName
+                                            )
+                                        }
+                                    >
+                                        {warehouse.warehouseName}
+                                    </NavDropdown.Item>
+                                ))}
+                        </NavDropdown>
+                    </Nav.Item>
                 </Nav>
             </Row>
             <Row className="mt-3">
