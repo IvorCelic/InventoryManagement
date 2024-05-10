f750e8b517bbd855d9532f0a7c47380f911623ed
Ivor Ćelić <ivorcelic@gmail.com>
2024-05-10 23:10:26 +0200

    GenerateQRs for all products FE

--- Frontend/src/pages/products/Products.jsx
+++ Frontend/src/pages/products/Products.jsx
@@ -11,165 +11,189 @@ import useLoading from "../../hooks/useLoading";
 import { saveAs } from "file-saver";
 
 export default function Products() {
-  const navigate = useNavigate();
-  const { showError } = useError();
+    const navigate = useNavigate();
+    const { showError } = useError();
 
-  const [products, setProducts] = useState();
-  const [totalProducts, setTotalProducts] = useState();
-  const [page, setPage] = useState(1);
-  const [condition, setCondition] = useState("");
-  const { showLoading, hideLoading } = useLoading();
+    const [products, setProducts] = useState();
+    const [totalProducts, setTotalProducts] = useState();
+    const [page, setPage] = useState(1);
+    const [condition, setCondition] = useState("");
+    const { showLoading, hideLoading } = useLoading();
 
-  async function fetchProducts() {
-    showLoading();
-    const responsePagination = await ProductService.GetPagination(
-      page,
-      condition
-    );
-    const responseProduct = await ProductService.get("Product");
-    if (!responsePagination.ok) {
-      showError(responsePagination.data);
-      return;
+    async function fetchProducts() {
+        showLoading();
+        const responsePagination = await ProductService.GetPagination(page, condition);
+        const responseProduct = await ProductService.get("Product");
+        if (!responsePagination.ok) {
+            showError(responsePagination.data);
+            return;
+        }
+
+        if (responsePagination.data.length == 0) {
+            setPage(page - 1);
+            return;
+        }
+        setProducts(responsePagination.data);
+        setTotalProducts(responseProduct.data.length);
+        hideLoading();
     }
 
-    if (responsePagination.data.length == 0) {
-      setPage(page - 1);
-      return;
+    async function generateQRCode(productId) {
+        showLoading();
+        const response = await ProductService.GenerateQRCode("QRCode", productId);
+
+        if (!response.ok) {
+            showError(response.data);
+            hideLoading();
+            return;
+        }
+
+        const blob = new Blob([response.data], { type: "application/pdf" });
+
+        // Trigger download
+        // saveAs(blob, "QRCodePDF.pdf");
+
+        // Trigger new window
+        const url = URL.createObjectURL(blob);
+        window.open(url, "_blank");
+
+        hideLoading();
     }
-    setProducts(responsePagination.data);
-    setTotalProducts(responseProduct.data.length);
-    hideLoading();
-  }
 
-  async function generateQRCodePDF(productId) {
-    showLoading();
-    const response = await ProductService.GenerateQRCodePDF("QRCode", productId);
+    async function generateAllQRCodes() {
+        showLoading();
+        const response = await ProductService.GenerateAllQRs("QRCode");
 
-    if (!response.ok) {
-      showError(response.data);
-      hideLoading();
-      return;
+        if (!response.ok) {
+            showError(response.data);
+            hideLoading();
+            return;
+        }
+
+        const blob = new Blob([response.data], { type: "application/pdf" });
+        const url = URL.createObjectURL(blob);
+        window.open(url, "_blank");
+
+        hideLoading();
     }
 
-    const blob = new Blob([response.data], { type: "application/pdf" });
+    useEffect(() => {
+        fetchProducts();
+    }, [page, condition]);
 
-    // Trigger download
-    // saveAs(blob, "QRCodePDF.pdf");
-
-    // Trigger new window
-    const url = URL.createObjectURL(blob);
-    window.open(url, "_blank");
-
-    hideLoading();
-  }
-
-  useEffect(() => {
-    fetchProducts();
-  }, [page, condition]);
-
-  async function removeProduct(id) {
-    showLoading();
-    const response = await ProductService.remove("Product", id);
-    showError(response.data);
-    if (response.ok) {
-      fetchProducts();
+    async function removeProduct(id) {
+        showLoading();
+        const response = await ProductService.remove("Product", id);
+        showError(response.data);
+        if (response.ok) {
+            fetchProducts();
+        }
+        hideLoading();
     }
-    hideLoading();
-  }
 
-  function isUnitary(product) {
-    if (product.isUnitary == null) return "Not defined";
-    if (product.isUnitary) return "Yes";
-    return "No";
-  }
+    function isUnitary(product) {
+        if (product.isUnitary == null) return "Not defined";
+        if (product.isUnitary) return "Yes";
+        return "No";
+    }
 
-  const totalPages = Math.ceil(totalProducts / 8);
+    const totalPages = Math.ceil(totalProducts / 8);
 
-  function handlePageChange(page) {
-    setPage(page);
-  }
+    function handlePageChange(page) {
+        setPage(page);
+    }
 
-  function handleSearch(searchTerm) {
-    setPage(1);
-    setCondition(searchTerm);
-  }
+    function handleSearch(searchTerm) {
+        setPage(1);
+        setCondition(searchTerm);
+    }
 
-  return (
-    <Container>
-      <Col>
-        <Row>
-          <SearchAndAdd
-            RouteName={RoutesNames.PRODUCTS_CREATE}
-            entity={"product"}
-            onSearch={handleSearch}
-          />
-        </Row>
-        <Row>
-          <Table striped bordered hover responsive>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Is Unitary</th>
-                {/* <th>Details</th> */}
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products &&
-                products.map((product, index) => (
-                  <tr key={index}>
-                    <td>{product.productName}</td>
-                    <td>{product.description}</td>
-                    <td>{isUnitary(product)}</td>
-                    {/* <td>
+    return (
+        <Container>
+            <Col>
+                <Row>
+                    <Col sm={9}>
+                        <SearchAndAdd
+                            RouteName={RoutesNames.PRODUCTS_CREATE}
+                            entity={"product"}
+                            onSearch={handleSearch}
+                        />
+                    </Col>
+                    <Col sm={3}>
+                        <Button
+                            className="btn btn-primary addButton"
+                            onClick={() => generateAllQRCodes()}
+                        >
+                            Generate all QR codes
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Table striped bordered hover responsive>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Is Unitary</th>
+                                {/* <th>Details</th> */}
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products &&
+                                products.map((product, index) => (
+                                    <tr key={index}>
+                                        <td>{product.productName}</td>
+                                        <td>{product.description}</td>
+                                        <td>{isUnitary(product)}</td>
+                                        {/* <td>
                                     <Button
                                         onClick={() => {
                                             navigate(`/products/details/${product.id}`);
                                         }}
                                     />
                                 </td> */}
-                    <td>
-                      <Container className="d-flex justify-content-center">
-                        <Button
-                          variant="link"
-                          className="actionButton"
-                          onClick={() => {
-                            navigate(`/products/${product.id}`);
-                          }}
-                        >
-                          <FaEdit size={25} />
-                        </Button>
-                        <Button
-                          variant="link"
-                          className="actionButton"
-                          title="Generate QR code"
-                          onClick={() => generateQRCodePDF(product.id)}
-                        >
-                          <FaQrcode size={25} />
-                        </Button>
-                        <Button
-                          variant="link"
-                          className="link-danger actionButton"
-                          onClick={() => removeProduct(product.id)}
-                        >
-                          <FaTrash size={25} />
-                        </Button>
-                      </Container>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
-        </Row>
-        <Row>
-          <MyPagination
-            currentPage={page}
-            totalPages={totalPages}
-            onPageChange={handlePageChange}
-          />
-        </Row>
-      </Col>
-    </Container>
-  );
+                                        <td>
+                                            <Container className="d-flex justify-content-center">
+                                                <Button
+                                                    variant="link"
+                                                    className="actionButton"
+                                                    onClick={() => {
+                                                        navigate(`/products/${product.id}`);
+                                                    }}
+                                                >
+                                                    <FaEdit size={25} />
+                                                </Button>
+                                                <Button
+                                                    variant="link"
+                                                    className="actionButton"
+                                                    title="Generate QR code"
+                                                    onClick={() => generateQRCode(product.id)}
+                                                >
+                                                    <FaQrcode size={25} />
+                                                </Button>
+                                                <Button
+                                                    variant="link"
+                                                    className="link-danger actionButton"
+                                                    onClick={() => removeProduct(product.id)}
+                                                >
+                                                    <FaTrash size={25} />
+                                                </Button>
+                                            </Container>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </Table>
+                </Row>
+                <Row>
+                    <MyPagination
+                        currentPage={page}
+                        totalPages={totalPages}
+                        onPageChange={handlePageChange}
+                    />
+                </Row>
+            </Col>
+        </Container>
+    );
 }
--- Frontend/src/services/ProductService.js
+++ Frontend/src/services/ProductService.js
@@ -1,42 +1,54 @@
 import {
-  get,
-  remove,
-  add,
-  edit,
-  getById,
-  httpService,
-  handleSuccess,
-  handleError,
+    get,
+    remove,
+    add,
+    edit,
+    getById,
+    httpService,
+    handleSuccess,
+    handleError,
 } from "./httpService";
 
 async function GetPagination(page, condition) {
-  return await httpService
-    .get("Product/SearchPagination/" + page + "?condition=" + condition)
-    .then((res) => {
-      return handleSuccess(res);
-    })
-    .catch((error) => {
-      return handleError(error);
-    });
+    return await httpService
+        .get("Product/SearchPagination/" + page + "?condition=" + condition)
+        .then((res) => {
+            return handleSuccess(res);
+        })
+        .catch((error) => {
+            return handleError(error);
+        });
 }
 
-async function GenerateQRCodePDF(name, productId) {
-  return await httpService
-    .get("/" + name + `?productId=${productId}`, { responseType: "arraybuffer" })
-    .then((response) => {
-      return handleSuccess(response);
-    })
-    .catch((error) => {
-      return handleError(error);
-    });
+async function GenerateQRCode(name, productId) {
+    return await httpService
+        .get("/" + name + `?productId=${productId}`, { responseType: "arraybuffer" })
+        .then((response) => {
+            return handleSuccess(response);
+        })
+        .catch((error) => {
+            return handleError(error);
+        });
+}
+
+async function GenerateAllQRs(name) {
+    return await httpService
+        .get("/" + name + "/GenerateAllQRs", { responseType: "arraybuffer" })
+        .then((response) => {
+            return handleSuccess(response);
+        })
+        .catch((error) => {
+            return handleError(error);
+        });
 }
 
 export default {
-  get,
-  remove,
-  add,
-  edit,
-  getById,
-  GetPagination,
-  GenerateQRCodePDF
+    get,
+    remove,
+    add,
+    edit,
+    getById,
+    GetPagination,
+    GenerateQRCode,
+    GenerateAllQRs,
 };
